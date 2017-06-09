eb713a83a2c919ad216d4491a567994265c69bbe
Peter Bakondy <pbakondy@gmail.com>
2017-06-09 13:48:28 +0200

    Upgrade dependencies

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
   entry: "./index.js",
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: "ios-device-list.min.js",
     library: "iosDeviceList",
     libraryTarget: "umd"
